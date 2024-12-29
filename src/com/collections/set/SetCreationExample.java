b8ae7e92ef20f9e285e4534926bb2b334889f404
dineshGitHub93 <dineshspachaiyappan93@gmail.com>
2024-12-29 20:48:11 +0530

    Set practice

---
+++ src/com/collections/set/SetCreationExample.java
@@ -0,0 +1,18 @@
+package com.collections.set;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SetCreationExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//Create a Set using HashSet 
+		Set<String> set = new HashSet<String>();
+		
+		//Displaying set 
+		System.out.println("Set Elements : "+set);
+	}
+
+}
