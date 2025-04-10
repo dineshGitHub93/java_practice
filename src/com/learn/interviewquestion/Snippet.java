61fbc686992703b45b512030576cae302e93413b
dineshGitHub93 <dineshspachaiyappan93@gmail.com>
2025-04-10 21:55:11 +0530

    practice interview qustion

--- src/com/learn/interviewquestion/Snippet.java
+++ src/com/learn/interviewquestion/Snippet.java
@@ -1,8 +1,17 @@
 package com.learn.interviewquestion;
 
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Snippet {
 	public static void main(String[] args) {
-		isAnagramUsingHashmap
+		
+		WebDriver driver = new ChromeDriver();
+		
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("arguments[0].click();", args);
+		
 	}
 }
 
